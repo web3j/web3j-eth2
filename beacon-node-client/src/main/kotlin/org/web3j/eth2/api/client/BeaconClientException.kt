84f2e96c4338a13060d80faa2e15e764fe6c8086
Xavier Arias <xavier@web3labs.com>
2020-11-13 09:09:02 +0100

    Move to api sub package

--- beacon-node-client/src/main/kotlin/org/web3j/eth2/api/client/BeaconClientException.kt
+++ beacon-node-client/src/main/kotlin/org/web3j/eth2/api/client/BeaconClientException.kt
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.eth2.client
+package org.web3j.eth2.api.client
 
 import org.web3j.eth2.api.schema.ErrorMessage
 import javax.ws.rs.ClientErrorException
--- beacon-node-client/src/main/kotlin/org/web3j/eth2/api/client/BeaconClientFactory.kt
+++ beacon-node-client/src/main/kotlin/org/web3j/eth2/api/client/BeaconClientFactory.kt
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.eth2.client
+package org.web3j.eth2.api.client
 
 import mu.KLogging
 import org.glassfish.jersey.client.proxy.WebResourceFactory
