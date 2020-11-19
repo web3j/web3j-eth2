5def6ecb961788e93c120a7448b152a047c3e09b
Xavier Arias <xavier@web3labs.com>
2020-11-19 16:38:21 +0100

    Fix test structure

--- beacon-node-client/src/test/kotlin/org/web3j/eth2/api/client/BeaconNodeApiTest.kt
+++ beacon-node-client/src/test/kotlin/org/web3j/eth2/api/client/BeaconNodeApiTest.kt
@@ -371,52 +371,52 @@ class BeaconNodeApiTest {
                 }
             }
         }
+    }
+
+    @Nested
+    @DisplayName("/node")
+    inner class NodeTest {
+
+        @Test
+        @DisplayName("GET /health")
+        fun `node should be healthy`() {
+            assertThat(client.node.health.status)
+                .isEqualTo(Response.Status.PARTIAL_CONTENT.statusCode)
+        }
+
+        @Test
+        @DisplayName("GET /identity")
+        fun `get node identity`() {
+            assertThat(client.node.identity.data.peerId).isNotEmpty()
+        }
+
+        @Test
+        @DisplayName("GET /version")
+        fun `get node version`() {
+            assertThat(client.node.identity.data.peerId).isNotEmpty()
+        }
+
+        @Test
+        @DisplayName("GET /syncing")
+        fun `get node syncing`() {
+            assertThat(client.node.syncing.data.syncDistance).isNotEmpty()
+        }
 
         @Nested
-        @DisplayName("/node")
-        inner class NodeTest {
+        @DisplayName("/peers")
+        inner class PeersTest {
 
             @Test
-            @DisplayName("GET /health")
-            fun `node should be healthy`() {
-                assertThat(client.node.health.status)
-                    .isEqualTo(Response.Status.PARTIAL_CONTENT.statusCode)
+            @DisplayName("GET /")
+            fun `find all node peers`() {
+                assertThat(client.node.peers.findAll().data).isNotEmpty()
             }
 
             @Test
-            @DisplayName("GET /identity")
-            fun `get node identity`() {
-                assertThat(client.node.identity.data.peerId).isNotEmpty()
-            }
-
-            @Test
-            @DisplayName("GET /version")
-            fun `get node version`() {
-                assertThat(client.node.identity.data.peerId).isNotEmpty()
-            }
-
-            @Test
-            @DisplayName("GET /syncing")
-            fun `get node syncing`() {
-                assertThat(client.node.syncing.data.syncDistance).isNotEmpty()
-            }
-
-            @Nested
-            @DisplayName("/peers")
-            inner class PeersTest {
-
-                @Test
-                @DisplayName("GET /")
-                fun `find all node peers`() {
-                    assertThat(client.node.peers.findAll().data).isNotEmpty()
-                }
-
-                @Test
-                @DisplayName("GET /{peer_id}")
-                fun `find node peers by ID`() {
-                    val peer = client.node.peers.findAll().data.first()
-                    assertThat(client.node.peers.findById(peer.id).data.id).isEqualTo(peer.id)
-                }
+            @DisplayName("GET /{peer_id}")
+            fun `find node peers by ID`() {
+                val peer = client.node.peers.findAll().data.first()
+                assertThat(client.node.peers.findById(peer.id).data.id).isEqualTo(peer.id)
             }
         }
     }
