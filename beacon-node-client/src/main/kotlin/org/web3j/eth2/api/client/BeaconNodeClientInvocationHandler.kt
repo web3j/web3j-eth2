8bc19c66d1c4ddde2d4c2cbb77409c652c385a63
Xavier Arias <xavier@web3labs.com>
2020-11-20 13:05:46 +0100

    Add missing event types and extract handler to separate file

---
+++ beacon-node-client/src/main/kotlin/org/web3j/eth2/api/client/BeaconNodeClientInvocationHandler.kt
@@ -0,0 +1,156 @@
+package org.web3j.eth2.api.client
+
+import mu.KLogging
+import org.web3j.eth2.api.schema.AttestationEvent
+import org.web3j.eth2.api.schema.BeaconEvent
+import org.web3j.eth2.api.schema.BeaconEventType
+import org.web3j.eth2.api.schema.BlockEvent
+import org.web3j.eth2.api.schema.ChainReorganizedEvent
+import org.web3j.eth2.api.schema.FinalizedCheckpointEvent
+import org.web3j.eth2.api.schema.HeadEvent
+import org.web3j.eth2.api.schema.VoluntaryExitEvent
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
+import java.lang.reflect.Proxy
+import java.net.URL
+import java.util.EnumSet
+import java.util.concurrent.CompletableFuture
+import java.util.function.Consumer
+import javax.ws.rs.WebApplicationException
+import javax.ws.rs.client.WebTarget
+import javax.ws.rs.sse.SseEventSource
+
+/**
+ * Invocation handler for proxied resources.
+ *
+ * Handles contract events using a Server-Sent Event (SSE) request.
+ *
+ * Also implements an exception mapping mechanism to avoid reporting
+ * [javax.ws.rs.ClientErrorException]s to the client.
+ */
+internal class BeaconNodeClientInvocationHandler(
+    private val target: WebTarget,
+    private val client: Any
+) : InvocationHandler {
+
+    override fun invoke(proxy: Any, method: Method, args: Array<out Any>?): Any? {
+        return if (method.isEvent()) {
+            logger.trace { "Invoking event method: $method" }
+            @Suppress("UNCHECKED_CAST")
+            invokeOnEvent(args!![0] as EnumSet<BeaconEventType>, args[1] as Consumer<BeaconEvent>)
+        } else {
+            logger.trace { "Invoking client method: $method" }
+            invokeClient(method, args)
+        }
+    }
+
+    private fun invokeOnEvent(
+        topics: EnumSet<BeaconEventType>,
+        onEvent: Consumer<BeaconEvent>
+    ): CompletableFuture<Void> {
+        val source = SseEventSource.target(clientTarget(topics)).build()
+        return SseEventSourceResult(source, onEvent).also {
+            it.open()
+        }
+    }
+
+    private fun invokeClient(method: Method, args: Array<out Any>?): Any {
+        try {
+            // Invoke the original method on the client
+            return method.invoke(client, *(args ?: arrayOf())).let {
+                if (Proxy.isProxyClass(it.javaClass)) {
+                    // The result is a Jersey web resource
+                    // so we need to wrap it again
+                    Proxy.newProxyInstance(
+                        method.returnType.classLoader,
+                        arrayOf(method.returnType),
+                        BeaconNodeClientInvocationHandler(target, it)
+                    )
+                } else {
+                    it
+                }
+            }
+        } catch (e: InvocationTargetException) {
+            throw handleException(e, method)
+        } catch (e: WebApplicationException) {
+            throw handleException(e, method)
+        }
+    }
+
+    private fun handleException(error: InvocationTargetException, method: Method): Throwable {
+        return error.targetException.let {
+            if (it is WebApplicationException) {
+                handleException(it, method)
+            } else {
+                logger.error {
+                    "Unexpected exception while invoking method $method: " +
+                            (error.message ?: error::class.java.canonicalName)
+                }
+                it
+            }
+        }
+    }
+
+    private fun handleException(error: WebApplicationException, method: Method): RuntimeException {
+        logger.error {
+            "Client exception while invoking method $method: " +
+                    (error.message ?: error.response.statusInfo.reasonPhrase)
+        }
+        return BeaconNodeException.of(error)
+    }
+
+    private fun clientTarget(topics: EnumSet<BeaconEventType>): WebTarget {
+        val resourcePath = client.toString()
+            .removePrefix("JerseyWebTarget { ")
+            .removeSuffix(" }")
+            .run { URL(this).path }
+        return target.path(resourcePath)
+            .queryParam("topics", *topics.toTypedArray())
+    }
+
+    private fun Method.isEvent() = parameterTypes.size == 2 &&
+            parameterTypes[0] == EnumSet::class.java &&
+            parameterTypes[1] == Consumer::class.java &&
+            returnType == CompletableFuture::class.java
+
+    private class SseEventSourceResult(
+        private val source: SseEventSource,
+        onEvent: Consumer<BeaconEvent>
+    ) : CompletableFuture<Void>() {
+        init {
+            source.register(
+                {
+                    val eventType: Class<out BeaconEvent> =
+                        when (BeaconEventType.fromString(it.name)) {
+                            BeaconEventType.HEAD -> HeadEvent::class.java
+                            BeaconEventType.BLOCK -> BlockEvent::class.java
+                            BeaconEventType.ATTESTATION -> AttestationEvent::class.java
+                            BeaconEventType.VOLUNTARY_EXIT -> VoluntaryExitEvent::class.java
+                            BeaconEventType.FINALIZED_CHECKPOINT -> FinalizedCheckpointEvent::class.java
+                            BeaconEventType.CHAIN_REORG -> ChainReorganizedEvent::class.java
+                        }
+                    onEvent.accept(it.readData(eventType) as BeaconEvent)
+                },
+                { completeExceptionally(it) },
+                { complete(null) }
+            )
+            whenComplete { _, _ ->
+                // Close the source gracefully by client
+                if (source.isOpen) source.close()
+            }
+        }
+
+        fun open() {
+            Thread {
+                source.open()
+                while (source.isOpen) {
+                    logger.debug { "Listening on SSE event source..." }
+                    Thread.sleep(5000)
+                }
+            }.start()
+        }
+    }
+
+    companion object : KLogging()
+}
