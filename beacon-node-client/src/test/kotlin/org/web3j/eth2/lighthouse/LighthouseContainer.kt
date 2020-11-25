e22532d4a64ca30af0c067a89c765114435a3642
Xavier Arias <xavier@web3labs.com>
2020-11-25 19:29:41 +0100

    Add Lighthouse container implementation

---
+++ beacon-node-client/src/test/kotlin/org/web3j/eth2/lighthouse/LighthouseContainer.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 Web3 Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.web3j.eth2.lighthouse
+
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.Wait
+
+class LighthouseContainer : GenericContainer<LighthouseContainer>("sigp/lighthouse:latest") {
+    init {
+        withCommand("lighthouse beacon --network medalla --http --http-address 0.0.0.0")
+        withLogConsumer { print(it.utf8String) }
+        withExposedPorts(5052, 9000)
+        waitingFor(
+            Wait.forHttp("/eth/v1/node/health")
+                .forStatusCode(206)
+                .forPort(5052)
+        )
+    }
+}
