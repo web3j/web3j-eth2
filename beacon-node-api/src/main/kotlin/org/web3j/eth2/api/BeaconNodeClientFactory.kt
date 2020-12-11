24c843d90bf318a352d97b6c78dc612ade64d2d4
Xavier Arias <xavier@web3labs.com>
2020-12-11 11:33:28 +0100

    Implement authentication to the Beacon client factory

--- beacon-node-api/src/main/kotlin/org/web3j/eth2/api/BeaconNodeClientFactory.kt
+++ beacon-node-api/src/main/kotlin/org/web3j/eth2/api/BeaconNodeClientFactory.kt
@@ -14,18 +14,56 @@ package org.web3j.eth2.api
 
 import org.glassfish.jersey.client.proxy.WebResourceFactory
 import java.lang.reflect.Proxy
+import java.util.Base64
+import javax.ws.rs.client.ClientRequestContext
+import javax.ws.rs.client.ClientRequestFilter
+import javax.ws.rs.client.WebTarget
+import javax.ws.rs.core.HttpHeaders
 
 /**
  * Factory class for building [BeaconNodeApi] instances.
+ *
+ * It supports [Basic](https://tools.ietf.org/html/rfc7617) and
+ * [Bearer](https://tools.ietf.org/html/rfc6750) authentication schemes.
  */
 object BeaconNodeClientFactory {
 
     /**
-     * Builds a [BeaconNodeApi] client with the given service.
+     * Builds a [BeaconNodeApi] client with the given service and no authentication.
+     *
+     * @param service the node service instance.
      */
     @JvmStatic
     fun build(service: BeaconNodeService): BeaconNodeApi {
-        val target = service.client.target(service.uri)
+        return build(service.client.target(service.uri))
+    }
+
+    /**
+     * Builds a [BeaconNodeApi] client with the given service and
+     * [Bearer authentication scheme](https://tools.ietf.org/html/rfc6750).
+     *
+     * @param service the node service instance.
+     * @param token the bearer authentication token.
+     */
+    @JvmStatic
+    fun build(service: BeaconNodeService, token: String): BeaconNodeApi {
+        return build(service.client.target(service.uri).register(OAuthFilter(token)))
+    }
+
+    /**
+     * Builds a [BeaconNodeApi] client with the given service and
+     * [Basic authentication scheme](https://tools.ietf.org/html/rfc7617).
+     *
+     * @param service the node service instance.
+     * @param username the Basic authentication username.
+     * @param password the Basic authentication password.
+     */
+    @JvmStatic
+    fun build(service: BeaconNodeService, username: String, password: String): BeaconNodeApi {
+        return build(service.client.target(service.uri).register(BasicAuthenticationFilter(username, password)))
+    }
+
+    private fun build(target: WebTarget): BeaconNodeApi {
         val client = WebResourceFactory.newResource(BeaconNodeApi::class.java, target)
         val handler = BeaconNodeClientInvocationHandler(target, client)
 
@@ -35,5 +73,21 @@ object BeaconNodeClientFactory {
             arrayOf(BeaconNodeApi::class.java),
             handler
         ) as BeaconNodeApi
+    }
+
+    private class BasicAuthenticationFilter(
+        private val username: String,
+        private val password: String
+    ) : ClientRequestFilter {
+        override fun filter(requestContext: ClientRequestContext) {
+            val credentials = Base64.getEncoder().encodeToString("$username:$password".toByteArray())
+            requestContext.headers.putSingle(HttpHeaders.AUTHORIZATION, "Basic $credentials")
+        }
+    }
+
+    private class OAuthFilter(private val token: String) : ClientRequestFilter {
+        override fun filter(requestContext: ClientRequestContext) {
+            requestContext.headers.putSingle(HttpHeaders.AUTHORIZATION, "Bearer $token")
+        }
     }
 }
